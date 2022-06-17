e7f61f929ca682d4f3fe1a0161c9fcb03cd2bda9
João Victor Pereira <pereiradossantosjoao1@gmail.com>
2022-06-17 19:24:41 -0300

    Initial commit

---
+++ Exercicios/praticaPython/index.py
@@ -0,0 +1,20 @@
+
+print('estou praticando Python')
+print('-'*40)
+n = float(input('Selecione um Número: '))
+n1 = float(input('Selecione outro Número: '))
+n2 = n+n1
+n3 = n*n1
+n4 = n/n1
+print(f'Os números somados dão {n2}')
+print(f'Os números multiplicados dão {n3}')
+print(f'Os números divididos dão o valor {n4}')
+print('-'*40)
+print('GUARDANDO DADOS EM LISTAS')
+dados = list[*]
+nome = str(input('Qual seu nome? '))
+idade = int(input('Qual a sua idade? '))
+peso = float(input('Qual seu peso? '))
+print()
+print()
+print(f'Nome do individuo é {nome}\nA idade do/a {nome} é {idade}\nO pesso de {nome} é {peso}')
---
+++ README.md
@@ -0,0 +1,2 @@
+# Html-Css
+ Curso de Html5 e Css3 do Curso em video
